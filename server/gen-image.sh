592f913d931d7aac69a672f89ebdd73eb42174eb
Aaron Watson <aaron@Aarons-MacBook.local>
2017-03-26 11:04:53 -0400

    remove weather-specific stuff

--- server/gen-image.sh
+++ server/gen-image.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")"
 
-python weather-script.py
-rsvg-convert --background-color=white -o weather-script-output.png weather-script-output.svg
-pngcrush -c 0 -ow weather-script-output.png
-cp -f weather-script-output.png ./weather-script-output.png
+python main.py
+rsvg-convert --background-color=white -o output.png output.svg
+pngcrush -c 0 -ow output.png
+cp -f output.png ./output.png
